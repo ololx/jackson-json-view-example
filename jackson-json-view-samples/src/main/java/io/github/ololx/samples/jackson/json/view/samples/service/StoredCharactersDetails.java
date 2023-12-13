d27d6fef06744608e6afcedcd109be7f2fecead1
Alexander A. Kropotin <ololx@icloud.com>
2023-12-13 22:40:58 +0200

    Update StoredCharactersDetails.java

--- jackson-json-view-samples/src/main/java/io/github/ololx/samples/jackson/json/view/samples/service/StoredCharactersDetails.java
+++ jackson-json-view-samples/src/main/java/io/github/ololx/samples/jackson/json/view/samples/service/StoredCharactersDetails.java
@@ -16,9 +16,9 @@ public class StoredCharactersDetails extends ConcurrentHashMap<Integer, Characte
 
     private static final Map<Integer, CharacterDetail> storedCharactersDetails = Map.of(
         1, new CharacterDetail(1, "Alan", "Sleep", 23, 173, 80.2),
-        2, new CharacterDetail(1, "Curious", "Sam", 30, 190, 100),
-        3, new CharacterDetail(1, "Duke", "McKen", 100, 200, 120),
-        4, new CharacterDetail(1, "Max", "Painkiller", 33, 173, 85.7)
+        2, new CharacterDetail(2, "Curious", "Sam", 30, 190, 100),
+        3, new CharacterDetail(3, "Duke", "McKen", 100, 200, 120),
+        4, new CharacterDetail(4, "Max", "Painkiller", 33, 173, 85.7)
     );
 
     public StoredCharactersDetails() {
